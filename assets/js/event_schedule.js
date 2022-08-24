f2bc906dd017ed978741a8f9c160d7594ab0b3bc
Matthew Ardizzone <pastman52@gmail.com>
2022-08-24 10:43:51 -0400

    fixed date

--- assets/js/event_schedule.js
+++ assets/js/event_schedule.js
@@ -74,7 +74,7 @@
                 
                 { name: "Garden City Hotel, Patio Bar",
                   where: "Garden City, NY",
-                  when: new Date(2022, Month.Sep, 3, 0, 0, 0, 0),
+                  when: new Date(2022, Month.Sep, 1, 0, 0, 0, 0),
                   time: "7-10pm",
                   link: "https://www.gardencityhotel.com/",
                   image: "garden_city_hotel.jpg"
